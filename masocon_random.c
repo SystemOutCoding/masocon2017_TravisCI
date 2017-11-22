c4cd8f75ea1c66db3e240b7d4fbd15a530ac7af2
Bae.JunHyun <praisedguy@gmail.com>
2017-11-22 07:15:39 +0000

    1설정시 0출력

--- masocon_random.c
+++ masocon_random.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+unsigned int _random(unsigned int maxValue);
+
 int main(int argc, char * argv[]){
 	unsigned int maxValue;
 	char interrupter;
+
 	printf("Input Max Value :");
 	scanf("%d", &maxValue);
-	while(interrupter != 'b'){
-	scanf("%c", &interrupter);
-	printf("%d", rand() % maxValue);
 	
+	while(interrupter != 'b'){
+		scanf("%c", &interrupter);
+		printf("%d", _random(maxValue));
 	}
 }
+
+unsigned int _random(unsigned int maxValue){
+	return((rand() % maxValue)+1);
+}
+
+
+
+
