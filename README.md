d67bae749cf83ccdf6f1c1be54855af7a81d58f4
Bae.JunHyun <praisedguy@gmail.com>
2017-11-22 09:33:09 +0000

    Random 추출기 완료, makefile 완료, 예제용 test.txt 첨부, .travis.yml

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 [![Build Status](https://travis-ci.org/SystemOutCoding/masocon2017_jenkins_Clanguage.svg?branch=master)](https://travis-ci.org/SystemOutCoding/masocon2017_jenkins_Clanguage)
 
-# masocon2017_jenkins_Clanguage
-masocon2017_jenkins_Clanguage
+위의 아이콘을 눌러 보시면 TravisCI 로그를 보실수 있습니다.
+
+# masocon2017_TravisCI
+masocon2017_TravisCI
+
+마소콘 경품추첨에 사용될 Random 추출기 입니다.
+
+https://travis-ci.org 에서 CI하였습니다.
--- masocon_random.c
+++ masocon_random.c
@@ -1,25 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned int _random(unsigned int maxValue);
+unsigned int nl(char * _argv);
+unsigned int _random(unsigned int cntAttendees);
+void lottery(unsigned int seed, char * _argv);
 
-int main(int argc, char * argv[]){
-	unsigned int maxValue;
+int main(int argc, char* argv[])
+{
+	unsigned int cntAttendees, seed;
+	char * _argv = argv[argc - 1];
 	char interrupter;
+ 	cntAttendees = nl(_argv);
 
-	printf("Input Max Value :");
-	scanf("%d", &maxValue);
+	printf("금일 참가한 인원윈 : %d명 입니다. \n", cntAttendees);
 	
-	while(interrupter != 'b'){
+	while(interrupter != 'e'){
+		seed = _random(cntAttendees);
+		lottery(seed, _argv);
 		scanf("%c", &interrupter);
-		printf("%d", _random(maxValue));
 	}
+
+	return 0;
 }
 
-unsigned int _random(unsigned int maxValue){
-	return((rand() % maxValue)+1);
+void lottery(unsigned int seed, char * _argv){
+	char buf[256];
+	FILE *ptr_file;
+	ptr_file = fopen(_argv,"r");
+	int i = 0;	
+	while(fgets(buf,256,ptr_file)!=NULL){
+		if(i == seed){
+			printf("%s", buf);
+		}
+		i++;
+	}
+	fclose(ptr_file);
 }
 
+unsigned int _random(unsigned int cntAttendees){
+	return (rand() % cntAttendees);	
+}
 
-
-
+unsigned int nl(char * _argv){
+	unsigned int nl = 0 ;
+	char buf[256];
+	int i = 0;
+	FILE *ptr_file;
+	printf("Target File : %s \n", _argv);	
+	ptr_file = fopen(_argv, "r");	
+	while (fgets(buf,256,ptr_file)!=NULL){
+		nl++;		
+	}
+	if(!ptr_file)	return -1;
+	fclose(ptr_file);
+	return nl;
+}
